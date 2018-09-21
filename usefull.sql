42f684d2b124cae1f5a38ea68d69aad05c0a186e
nworr <martin@observatoire-environnement.org>
2018-09-21 17:37:28 +0200

    nouveau fichier

---
+++ usefull.sql
@@ -0,0 +1,7 @@
+with tvue 
+as (     select * from pg_views
+union
+    select schemaname, matviewname, matviewowner, definition from pg_matviews             )
+select * from tvue
+where definition like '%<NomTable>%'
+-- test de l'appel d'une table <NomTable> par une vue (materialisée ou  non )
