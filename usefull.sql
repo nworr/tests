cee5d86b9763513ee8ba2ec4eb5266473a4d89e0
nworr <martin@observatoire-environnement.org>
2018-10-17 10:26:55 +0200

    Update usefull.sql

--- usefull.sql
+++ usefull.sql
@@ -1,7 +1,13 @@
+-- test de l'appel d'une table <NomTable> par une vue (materialisée ou  non )
 with tvue 
 as (     select * from pg_views
 union
     select schemaname, matviewname, matviewowner, definition from pg_matviews             )
 select * from tvue
 where definition like '%<NomTable>%'
--- test de l'appel d'une table <NomTable> par une vue (materialisée ou  non )
+
+
+-- pour cherche une table dans tous les schema
+ select * from pg_tables where tablename like '%<Recherche>%'
+ 
+ 
